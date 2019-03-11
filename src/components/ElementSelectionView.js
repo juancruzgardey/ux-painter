52a1b52d617e9d49cca9ab7e882fcb90b81ae259
juancruzgardey <juancruzgardey@hotmail.com>
2019-03-11 16:22:59 -0300

    add target element image

--- src/components/ElementSelectionView.js
+++ src/components/ElementSelectionView.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PageSelector from '../PageSelector';
 import XPathInterpreter from '../refactorings/XPathInterpreter';
 import {goBack, Link} from 'route-lite';
+import domtoimage from 'dom-to-image';
 
 class ElementSelectionView extends React.Component {
 
@@ -10,7 +11,7 @@ class ElementSelectionView extends React.Component {
         this.state = {elementXpath: ""};
         this.refactoring = new this.props.refactoring();
         this.pageSelector = new PageSelector (this);
-        this.pageSelector.enableElementSelection({clea
+        this.pageSelector.enableElementSelection({
             "scrapperClass": "QuerySelectorScrapper",
             "targetElementSelector": this.refactoring.targetElements(),
             "onElementSelection": "onElementSelection",
@@ -20,6 +21,7 @@ class ElementSelectionView extends React.Component {
 
         this.disableElementSelection = this.disableElementSelection.bind(this);
         this.handleBack = this.handleBack.bind(this);
+        this.targetElementImage = React.createRef();
     }
 
 
@@ -27,6 +29,12 @@ class ElementSelectionView extends React.Component {
         const elementXpath = (new XPathInterpreter()).getPath(anElement, document.body)[0];
         this.setState({elementXpath: elementXpath});
         this.refactoring.setElement(elementXpath);
+        const me = this;
+        domtoimage.toPng(anElement, {width: 300, height: 200}).then(function (dataUrl) {
+            me.targetElementImage.current.src = dataUrl;
+        }).catch(function (error) {
+            console.error('oops, something went wrong!', error);
+        });
     }
 
     disableElementSelection() {
@@ -44,7 +52,8 @@ class ElementSelectionView extends React.Component {
             <div className={"col-md-12"}>
                 <h2>Select an Element</h2>
                 <div className={'form-group'}>
-                    <p>Target Element <span>{this.state.elementXpath}</span></p>
+                    <p>Target Element</p>
+                    <img className={'target_element_image'} ref={this.targetElementImage}/>
                 </div>
                 <div className={'form-group'}>
                     <Link onClick={this.disableElementSelection} className={'btn btn-warning'} component={this.refactoring.getView()} componentProps={{"refactoring": this.refactoring}}>Continue</Link>
--- src/components/RefactoringOnElementView.js
+++ src/components/RefactoringOnElementView.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import RefactoringView from './RefactoringView';
+import domtoimage from "dom-to-image";
 
 class RefactoringOnElementView extends React.Component {
 
     constructor (props) {
         super(props);
+        this.targetElementImage = React.createRef();
+        const me = this;
+        domtoimage.toPng(this.props.refactoring.getElement(), {width: 300, height: 200}).then(function (dataUrl) {
+            me.targetElementImage.current.src = dataUrl;
+        }).catch(function (error) {
+            console.error('oops, something went wrong!', error);
+        });
     }
+
 
     render () {
         return (
             <RefactoringView refactoring={this.props.refactoring}>
                 <div className={'form-group'}>
-                    <label>Target Element</label>
-                    <p>{this.props.refactoring.getElementXpath()}</p>
+                    <p>Target Element</p>
+                    <img className={'target_element_image'} ref={this.targetElementImage}/>
                 </div>
                 {this.props.children}
             </RefactoringView>
