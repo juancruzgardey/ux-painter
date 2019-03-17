d6f259228625b4678866543afba6cf7513d062f7
juancruzgardey <juancruzgardey@hotmail.com>
2019-03-17 17:40:22 -0300

    fix style of AddAutocomplete refactoring

--- src/content.js
+++ src/content.js
@@ -2,7 +2,6 @@
 /* src/content.js */
 import React from 'react';
 import Frame, { FrameContextConsumer } from 'react-frame-component';
-import "bootstrap/dist/css/bootstrap.css";
 import "./content.css";
 import RefactoringListView from './components/RefactoringListView';
 import Router from "route-lite";
@@ -12,7 +11,7 @@ class Main extends React.Component {
     render() {
         return (
             <Frame head={[<link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/1.chunk.css")} ></link>,
-                <link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/0.chunk.css")} ></link>]}>
+                <link type="text/css" rel="stylesheet" href={'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'} ></link>]}>
                 <FrameContextConsumer>
                     {
                         // Callback is invoked with iframe's window and document instances
