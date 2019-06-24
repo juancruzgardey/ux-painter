67c03b21d7e812fe6dc079ebb879eba25bbf9925
Juan Cruz Gardey <juancruzgardey@hotmail.com>
2019-06-24 14:50:24 -0300

    added new styles to Turn Input into Select style.

--- src/previewers/TurnInputIntoSelectPreviewer.js
+++ src/previewers/TurnInputIntoSelectPreviewer.js
@@ -7,13 +7,26 @@ class TurnInputIntoSelectPreviewer extends Previewer {
 
         const targetElementContainer = this.pageSegmentator.findPageSegmentOfElement(aRefactoring.getElement());
         let existingSelect = document.querySelector("select");
+        let widget;
+        // if a select input already exists, border and colors properties are fetched from that input
         if (existingSelect) {
-            existingStyle.border = window.getComputedStyle(existingSelect).getPropertyValue("border");
-            existingStyle.color = window.getComputedStyle(existingSelect).getPropertyValue("color");
-            existingStyle["background-color"] = window.getComputedStyle(existingSelect).getPropertyValue("background-color");
-            existingStyle.width = window.getComputedStyle(aRefactoring.getElement()).getPropertyValue("width");
-            existingStyle.height = window.getComputedStyle(aRefactoring.getElement()).getPropertyValue("height");
+            widget = existingSelect;
+            // copy arrow appearance from the other select
+            existingStyle["background"] = window.getComputedStyle(widget).getPropertyValue("background");
+            existingStyle["-webkit-appearance"] = window.getComputedStyle(widget).getPropertyValue("-webkit-appearance");
         }
+        else {
+            widget = document.querySelector("input[type='text']");
+        }
+        existingStyle.border = window.getComputedStyle(widget).getPropertyValue("border");
+        existingStyle["border-radius"] = window.getComputedStyle(widget).getPropertyValue("border-radius");
+        existingStyle.padding = window.getComputedStyle(widget).getPropertyValue("padding");
+        existingStyle.margin = window.getComputedStyle(widget).getPropertyValue("margin");
+        existingStyle.color = window.getComputedStyle(widget).getPropertyValue("color");
+
+        // keep the original size of the free input
+        existingStyle.width = window.getComputedStyle(aRefactoring.getElement()).getPropertyValue("width");
+        existingStyle.height = window.getComputedStyle(aRefactoring.getElement()).getPropertyValue("height");
 
         let previewRefactoring = this.cloneRefactoring(aRefactoring);
         previewRefactoring.setStyle(existingStyle);
