089e65de3e94a5e3ff2c5ac2f86cb96fd79ea779
juancruzgardey <juancruzgardey@hotmail.com>
2019-09-18 13:07:21 +0200

    added an icon to each refactoring in the catalogue to show its description

--- src/components/RefactoringListView.js
+++ src/components/RefactoringListView.js
@@ -1,5 +1,10 @@
 import {Link, goBack} from "route-lite";
 import React from "react";
+import 'tipr/tipr/tipr.css';
+const $ = require('jquery');
+//global.jQuery = $;
+window.jQuery = $;
+require ('tipr');
 
 class RefactoringListView extends React.Component {
 
@@ -7,14 +12,21 @@ class RefactoringListView extends React.Component {
         super(props);
     }
 
+
     render () {
         const me = this;
         const listItems = window.refactoringManager.getRefactoringCatalogue().map((refactoringClass, i) => {
             let refactoring = new refactoringClass();
-            return <li><Link component={me.nextComponent(refactoring)} componentProps={{refactoring: refactoring}}>{refactoringClass.asString()}</Link></li>
+            return (
+            <li>
+                <Link component={me.nextComponent(refactoring)} componentProps={{refactoring: refactoring}}>{refactoringClass.asString()}</Link>
+                <a className={'uxpainter-refactoring-info tip'} data-tip="refactoring description">
+                    <i className="fas fa-info-circle fa-lg"></i>
+                </a>
+            </li>)
         });
         return (
-            <div className={"row col-12"}>
+            <div className={"row"}>
                 <h5 className={'text-center'}>Available Refactorings</h5>
                 <div className={'row col-12'}>
                     <ul>
--- src/content.js
+++ src/content.js
@@ -11,9 +11,12 @@ class Main extends React.Component {
     render() {
         return (
             <div className={'uxpainter-frame'} style={{height: "100%"}}>
-            <Frame head={[<link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/0.chunk.css")} ></link>,
+            <Frame head={[
+                <link type="text/css" rel="stylesheet" href={'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'}></link>,
+                <link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/0.chunk.css")} ></link>,
                 <link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/1.chunk.css")} ></link>,
-                <link type="text/css" rel="stylesheet" href={'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'} ></link>]}>
+                <link type="text/css" rel="stylesheet" href={'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css'}></link>
+            ]}>
                 <FrameContextConsumer>
                     {
                         // Callback is invoked with iframe's window and document instances
