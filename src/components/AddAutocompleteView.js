6dc7b21baca9c1502c88d1ebfd9f3f6a13c13f8d
juancruzgardey <juancruzgardey@hotmail.com>
2021-03-18 15:01:41 -0300

    fix Add Autocomplete configuration view

--- src/components/AddAutocompleteView.js
+++ src/components/AddAutocompleteView.js
@@ -8,11 +8,17 @@ class AddAutocompleteView extends React.Component {
         this.handleChange = this.handleChange.bind(this);
     }
 
+    handleChange(event) {
+        const valuesCollection = event.target.value.split(",");
+        this.setState({values: valuesCollection});
+        this.props.refactoring.setValues(valuesCollection);
+    }
+
 
 
     render() {
         return (
-            <RefactoringConfigurationView refactoring={this.refactoring}>
+            <RefactoringConfigurationView refactoring={this.props.refactoring}>
                 <div className={'form-group'}>
                     <label>Values to autocomplete</label>
                     <input type={'text'} placeholder={'apple,orange,banana'} className={'form-control'} onChange={this.handleChange}/>
